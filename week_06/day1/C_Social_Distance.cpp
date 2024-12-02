0a86a671e20ca649b79b7e628736eb840a467b5d
shubho-saha <shuvo7242@gmail.com>
2024-12-02 22:27:46 +0600

    xpsc week6 day1

---
+++ week_06/day1/C_Social_Distance.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+
+void solve2(){
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    int cnt=0;
+    int l=0,r=0;
+    while(r<n){
+        if(s[r]=='1'){
+            r+=(k+1);
+            l=r;
+        }
+        else if(r-l==k){
+            cnt++;
+            r++;
+            l=r;
+        }
+        else if(s[r]=='0'){
+            r++;
+            if(r==n) cnt++;
+        }
+    }
+    cout<<cnt<<endl;
+}
+
+void solve()
+{
+    int n, k;
+    cin>>n>>k;
+    string str;
+    cin>>str;
+
+    vector<int> gap;
+    int left = 0;
+    bool oneFound = false;
+    for(int i=0; i<n; i++) {
+        if (str[i] == '1') {
+            if (gap.size() == 0) {
+                gap.push_back(i-left);
+                oneFound = true;
+                left = i;
+            } else {
+                gap.push_back(i-left-1);
+                oneFound = true;
+                left = i;
+            }     
+        }
+    }
+
+    if (!oneFound) {
+        int availGap = n-1;
+        int cnt = 1;
+        int requireGap = k+1;
+        while(availGap >= requireGap) {
+            cnt++;
+            availGap -= (k+1);
+        }
+        cout<<cnt<<nl;
+        return;
+    }
+
+    int cnt = 0;
+    int requireGap;
+    for(int i=0; i<gap.size(); i++) {
+        int availGap = gap[i];
+        if (i==0) {
+            requireGap = k+1;
+        } else {
+            requireGap = 2*k + 1;
+        }
+        
+        while(availGap >= requireGap){
+            cnt++;
+            availGap -= (k+1);
+        }         
+    }
+    
+    if (str[n-1] != '1') {
+        int availGap = n-1-left;
+        int requireGap = k + 1;
+        while(availGap >= requireGap)
+        {
+            cnt++;
+            availGap -= (k+1);
+        }
+    }
+
+    cout<<cnt<<nl;
+
+}
+int main()
+{
+    fastIO();
+    int t = 1; 
+    cin>>t;
+    while(t--)
+    {
+        // solve();
+        solve2();
+    }
+
+    return 0;
+}
