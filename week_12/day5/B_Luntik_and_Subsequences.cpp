086b73245a742a217473cfe74fdccc53d8254081
shubho-saha <shuvo7242@gmail.com>
2025-01-27 00:37:37 +0600

    xpsc week12 day5

---
+++ week_12/day5/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+
+void solve()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    ll one = 0, zero=0;
+    for(int i=0; i<n; i++) {
+        cin>>v[i];
+        if (v[i] == 0) zero++;
+        if (v[i] == 1) one++;
+    }
+
+    cout<<((1ll<<zero) * (one))<<nl;
+}
+int main()
+{
+    fastIO();
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
