a7e46b92ab5fc0d4fff6ba7d0a39bcc618124525
shubho-saha <shuvo7242@gmail.com>
2025-02-18 03:57:01 +0600

    xpsc week15 day2

---
+++ week_15/day2/40_Combination_Sum_II_leetcode.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+
+void gen(vector<int> &v, int idx, int need, vector<vector<int>> &res, vector<int>&sub)
+{
+    if (need == 0) {
+        res.push_back(sub);
+        return;
+    }
+    if (idx == v.size()) return;
+
+    if (v[idx] <= need) {
+        sub.push_back(v[idx]);
+        gen(v, idx+1, need-v[idx], res, sub);
+        sub.pop_back();
+        while(idx+1 < v.size() && v[idx] == v[idx+1]) {
+            idx++;
+        }
+        gen(v, idx+1, need, res, sub);
+    }
+}
+int main()
+{
+    fastIO();
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(auto &it : v) cin>>it;
+
+    int target; cin>>target;
+    
+    sort(v.begin(), v.end());
+    vector<vector<int>> res;
+    vector<int> sub;
+    gen(v, 0, target, res, sub);
+
+    for(auto vec : res) {
+        for(auto el : vec) {
+            cout<<el<<" ";
+        }
+        cout<<nl;
+    }
+
+
+    return 0;
+}
